208a1d1c259b32bcba071076ae7e13aba992ed95
Puncoz Nepal <puncoz138@gmail.com>
2018-08-02 17:38:36 +0545

    pusher js setup in react part

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import MediaHandler       from "./../MediaHandler";
+import Pusher             from "pusher-js";
+import Peer               from "simple-peer";
 
 class App extends Component {
     constructor() {
@@ -10,12 +12,23 @@ class App extends Component {
             otherUserId: null,
         };
 
+        this.user = window.user;
+        this.user.stream = null;
+        this.peers = {};
+
         this.mediaHandler = new MediaHandler();
+
+        this.setupPusher();
+
+        this.callTo = this.callTo.bind(this);
+        this.setupPusher = this.setupPusher.bind(this);
+        this.startPeer = this.startPeer.bind(this);
     }
 
     componentWillMount() {
         this.mediaHandler.getPermissions().then(stream => {
             this.setState({hasMedia: true});
+            this.user.stream = stream;
 
             try {
                 this.myVideo.srcObject = stream;
@@ -27,6 +40,73 @@ class App extends Component {
         });
     }
 
+    setupPusher() {
+        this.pusher = new Pusher(APP_KEY, {
+            authEndpoint: "/pusher/auth",
+            cluster: "ap2",
+            auth: {
+                params: this.user.id,
+                header: {
+                    "X-CSRF-Token": window.csrfToken,
+                },
+            },
+        });
+
+        this.channel = this.pusher.subscribe("presence-video-channel");
+
+        this.channel.bind(`client-signal-${this.user.id}`, signal => {
+            let peer = this.peers[signa.userId];
+
+            // if peer is not already exists, its an incoming call.
+            if (peer === undefined) {
+                this.setState({
+                    otherUserId: signal.userId,
+                });
+
+                peer = this.startPeer(signal.userId, false);
+            }
+        });
+    }
+
+    startPeer(peerId, isInitiator = true) {
+        const peer = new Peer({
+            initiator: isInitiator,
+            stream: this.user.stream,
+            trickle: false,
+        });
+
+        peer.on("signal", data => {
+            this.channel.trigger(`client-signal-${peerId}`, {
+                type: "signal",
+                userId: this.user.id,
+                data: data,
+            });
+        });
+
+        peer.on("stream", stream => {
+            try {
+                this.userVideo.srcObject = stream;
+            } catch (e) {
+                this.userVideo.src = URL.createObjectURL(stream);
+            }
+
+            this.userVideo.play();
+        });
+
+        peer.on("close", () => {
+            let peer = this.peers[peerId];
+            if (peer !== undefined) {
+                peer.destroy();
+            }
+
+            this.peers[peerId] = undefined;
+        });
+    }
+
+    callTo(userId) {
+        this.peers[userId] = this.startPeer(userId);
+    }
+
     render() {
         return (
             <div className="container">
@@ -36,9 +116,13 @@ class App extends Component {
                             <div className="card-header">My Video</div>
 
                             <div className="card-body">
+                                {[1, 2, 3, 4].map(userId => (
+                                    <button onClick={() => this.callTo(userId)}>Call User {userId}</button>
+                                ))}
+
                                 <div className="video-container">
-                                    <video className="my-video" ref={ref => {this.myVideo = ref;}}></video>
-                                    <video className="user-video" ref={ref => {this.userVideo = ref;}}></video>
+                                    <video className="my-video" ref={ref => {this.myVideo = ref;}}/>
+                                    <video className="user-video" ref={ref => {this.userVideo = ref;}}/>
                                 </div>
                             </div>
                         </div>
